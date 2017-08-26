ef9c55fd2af8c1fcf9ba0b13f65c77a03854a328
Zheng Luo <helloworld09@163.com>
2017-08-26 19:36:34 +0800

    Add: train test split

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 import logging
+import random
 from keras.models import load_model
+from sklearn.model_selection import train_test_split
 from neuralnets.BiLSTMCRF import BiLSTMCRF
 from neuralnets.keraslayers.ChainCRF import create_custom_objects
 from util.data import Data
@@ -14,14 +16,15 @@ inputPath = 'data/family/train.txt'
 data = Data(inputPathList=[inputPath])
 data.loadCoNLL(inputPath)
 
-X1 = data.sentences
-X2 = data.charSentences
+X = data.sentences
 y = data.labels
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
 
 modelWrapper = BiLSTMCRF(data)
 model = modelWrapper.buildModel()
 
-model.fit(X1, y)
+model.fit(X_train, y_train, epochs=5, validation_split=0.1, shuffle=True)
 model.save('model.h5')
 
 model = load_model('model.h5', custom_objects=create_custom_objects())
