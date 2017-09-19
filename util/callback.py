adffbbe1148a996c4550dc40c7f14b632d68dddb
Zheng Luo <helloworld09@163.com>
2017-09-19 08:21:51 +0800

    Update: save model

--- util/callback.py
+++ util/callback.py
@@ -1,25 +1,35 @@
+import os
 import logging
+import time
 from keras.callbacks import Callback
 from util.metric import categorical_metric
 
 
 class metricHistory(Callback):
 
-    def __init__(self, X_val=None, y_val=None):
+    def __init__(self, X_val=None, y_val=None, save=True):
         super(metricHistory, self).__init__()
         self.metric_history = []
         self.X_val = X_val
         self.y_val = y_val
         self.model = None
         self.params = {}
+        self.dir = 'h5/'
+        self.save = save
 
     def set_params(self, params):
         self.params.update(params)
+
+    def on_train_begin(self, logs=None):
+        dirName = time.strftime("%m-%d_%H:%M", time.localtime())
+        self.dir = self.dir + dirName + '/'
+        os.mkdir(self.dir)
 
     def on_epoch_end(self, epoch, logs={}):
         y_predict = self.model.predict(self.X_val)
         y_predict = y_predict.argmax(axis=-1)
         metric = categorical_metric(self.y_val, y_predict)
+        if self.save and self.metric_history and metric > max(self.metric_history):
+            self.model.save('{}epoch{}_acc{}.h5'.format(self.dir, epoch, metric))
         self.metric_history.append(metric)
-        logging.info(str(metric))
-        self.model.save('h5/epoch{}_acc{}.h5'.format(epoch, metric))
+        logging.info('Metric: ' + str(metric))
